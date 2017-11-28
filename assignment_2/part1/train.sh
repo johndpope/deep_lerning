b0648d247cd6e130299d4528757657137f5f3be2
Martin Riva <sdemo133@int2.bullx>
2017-11-28 02:36:30 +0100

    asdad

--- assignment_2/part1/train.sh
+++ assignment_2/part1/train.sh
@@ -9,4 +9,4 @@ module load python/3.5.2
 module load cuda/8.0.44
 module load cudnn/8.0-v6.0
 
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test5 --learning_rate 0.0025 --model_type LSTM --input_length 15
+python3 train.py --print_every 50 --summary_path ./summaries/test35 --learning_rate 0.0025 --model_type RNN --input_length 35
