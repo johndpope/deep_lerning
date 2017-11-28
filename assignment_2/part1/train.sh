f8f44d1c96d5a1695a2a9c41bf9c443a802a9ed3
mrivar <martin7557@gmail.com>
2017-11-28 02:31:20 +0100

    more

--- assignment_2/part1/train.sh
+++ assignment_2/part1/train.sh
@@ -10,12 +10,3 @@ module load cuda/8.0.44
 module load cudnn/8.0-v6.0
 
 python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test5 --learning_rate 0.0025 --model_type LSTM --input_length 5
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test10 --learning_rate 0.0025 --model_type LSTM --input_length 10
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test15 --learning_rate 0.0025 --model_type LSTM --input_length 15
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test20 --learning_rate 0.0025 --model_type LSTM --input_length 20
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test25 --learning_rate 0.0025 --model_type LSTM --input_length 25
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test30 --learning_rate 0.0025 --model_type LSTM --input_length 30
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test35 --learning_rate 0.0025 --model_type LSTM --input_length 35
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test40 --learning_rate 0.0025 --model_type LSTM --input_length 40
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test45 --learning_rate 0.0025 --model_type LSTM --input_length 45
-python3 train.py --print_every 50 --summary_path ./summaries/LSTM/test50 --learning_rate 0.0025 --model_type LSTM --input_length 50
