900aaaf0c0d91f9aaa29c617317da86c5fdef453
Martin Riva <sdemo133@int2.bullx>
2017-11-28 13:12:46 +0100

    part2 run

---
+++ assignment_2/part2/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -p gpu
+#SBATCH -t 01:00:00
+#SBATCH -o fibonacci_%A.output
+#SBATCH -e fibonacci_%A.error
+
+module load python/3.5.2
+module load cuda/8.0.44
+module load cudnn/8.0-v6.0
+
+python3 train.py --txt_file books/book_EN_democracy_in_the_US.txt
