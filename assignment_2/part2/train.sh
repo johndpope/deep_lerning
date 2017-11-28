f29e5f86c6c47475db0baec50e7102818b53bfb5
Martin Riva <sdemo133@int2.bullx>
2017-11-28 20:43:38 +0100

    asdasd

--- assignment_2/part2/train.sh
+++ assignment_2/part2/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -p gpu
-#SBATCH -t 01:00:00
+#SBATCH -t 02:30:00
 #SBATCH -o fibonacci_%A.output
 #SBATCH -e fibonacci_%A.error
 
@@ -9,4 +9,4 @@ module load python/3.5.2
 module load cuda/8.0.44
 module load cudnn/8.0-v6.0
 
-python3 train.py --txt_file books/book_EN_democracy_in_the_US.txt
+python3 train.py --txt_file books/book_EN_democracy_in_the_US.txt --print_every 200
